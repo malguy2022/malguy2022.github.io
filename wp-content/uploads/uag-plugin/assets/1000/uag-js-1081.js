926c7715b976710c6069cd0a6918269edd879589
malred <13695024677@163.com>
2024-08-22 09:19:25 +0800

    24-8-22

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1081.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'DOMContentLoaded', function() {
+	UAGBInlineNotice.init( {"c_id":null,"cookies":false,"close_cookie_days":1,"noticeDismiss":"","icon":"rectangle-xmark"}, '.uagb-block-a84b53c9' );
+});
+window.addEventListener( 'DOMContentLoaded', function() {
+	UAGBInlineNotice.init( {"c_id":null,"cookies":false,"close_cookie_days":1,"noticeDismiss":"","icon":"rectangle-xmark"}, '.uagb-block-c06e7aeb' );
+});
+ });
